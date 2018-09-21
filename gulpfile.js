c711b1c8eafb32937cbd5dc6856873f936c007f1
root <root@shion-pc.localdomain>
2018-09-21 17:17:12 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,10 @@ var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
 var zip = require('gulp-zip');
 
+var order = require('gulp-order');
+var concat = require('gulp-concat');   //合并文件
+var rename = require('gulp-rename');   //文件重命名
+
 // postcss plugins
 var autoprefixer = require('autoprefixer');
 var colorFunction = require('postcss-color-function');
@@ -24,7 +28,7 @@ var nodemonServerInit = function () {
     livereload.listen(1234);
 };
 
-gulp.task('build', ['css'], function (/* cb */) {
+gulp.task('build', ['css','js'], function (/* cb */) {
     return nodemonServerInit();
 });
 
@@ -46,11 +50,25 @@ gulp.task('css', function () {
         .pipe(livereload());
 });
 
+//JS处理
+gulp.task('js',function(){
+    return gulp.src(['/assets/js/jquery-min.js','/assets/js/bootstrap.min.js','/assets/js/popper.min.js'])  //选择合并的JS
+        .pipe(order([
+            '/assets/js/jquery-min.js',
+            '/assets/js/popper.min.js',
+            '/assets/js/bootstrap.min.js',
+        ]))
+        .pipe(concat('all.js'))   //合并js
+        .pipe(rename({suffix:'.min'}))     //重命名
+        //.pipe(uglify())                    //压缩
+        .pipe(gulp.dest('assets/built'))            //输出 
+ });
+
 gulp.task('watch', function () {
     gulp.watch('assets/css/**', ['css']);
 });
 
-gulp.task('zip', ['css'], function () {
+gulp.task('zip', ['css,js'], function () {
     var targetDir = 'dist/';
     var themeName = require('./package.json').name;
     var filename = themeName + '.zip';
